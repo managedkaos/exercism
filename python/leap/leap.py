4348390b5b175fb167d986b6330085db7c1e47ed
Michael Jenkins <michael.jenkins@disney.com>
2016-11-04 15:43:05 -0700

    Submit final answer for leap year

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -3,21 +3,11 @@ def is_leap_year(year):
     if not year:
         return False
 
-    mod4   = year % 4
-    mod100 = year % 100
-    mod400 = year % 400
-
-    if (mod4==0):
-        if (mod100==0):
-            if (mod400==0):
-                return True
-
-    if (mod4==0) and (mod400==0):
+    if (year % 4) == 0 and (year % 100) != 0 or (year % 400 == 0):
         return True
-
-    return False
+    else: 
+        return False
 
 if __name__ == '__main__':
     import sys
-    result = is_leap_year(int(sys.argv[1]))
-    print result
+    print is_leap_year(int(sys.argv[1]))
